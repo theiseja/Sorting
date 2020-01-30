52c930966bfaf05cf16c84b924514b4dfd0971a0
Jesse Theisen <theiseja@gmail.com>
2020-01-29 22:41:47 -0600

    Adds blank functions in iterative_sorting.js for tomorrow

--- src/iterative_sorting/iterative_sorting.js
+++ src/iterative_sorting/iterative_sorting.js
@@ -2,4 +2,15 @@
 
 function selection_sort(arr) {
     // TODO
+    let cur_index = i
+    smallest_index = cur_index
+    // TODO: Find next smallest element
+    // can be done in 3 loc
+    for (i = 0; i < cur_index.length; i++) {
+
+    }
+}
+
+function bubble_sort(arr) {
+    
 }
